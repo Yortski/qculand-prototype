cc2ffe684553ec8d41d80c1c40eb7dd834a4570f
Bitterbuggg <salamat.remie.obregon@gmail.com>
2025-11-11 20:34:01 +0800

    forgor

--- src/components/scenes/LandingScene/PlayerUI.jsx
+++ src/components/scenes/LandingScene/PlayerUI.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-
-
-import React from 'react';
 import { motion } from 'framer-motion';
-
 
 export default function PlayerUI() {
   return (
